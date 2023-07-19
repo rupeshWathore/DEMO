575b177060599b5fcefd9473b82002ed41f17495
rupeshWathore <rupeshwathore@gmail.com>
2023-07-19 22:56:12 +0530

    [rupeshWathore]THIS IS DEMO

---
+++ src/encapsulation/EncapsulationUse.java
@@ -0,0 +1,18 @@
+package encapsulation;
+
+public class EncapsulationUse {
+
+	public static void main(String[] args) 
+	{
+		Sample  S1=new Sample();
+		S1.setRollNo(25);
+		S1.setName("Rupesh");
+		
+		System.out.println(S1.getRollNo());
+	    System.out.println(S1.getName());
+		
+		
+
+	}
+
+}
